5c8eaaf2ee55b50c76e58318e8f8909ba89697b7
Victor Marques <victor14souza@gmail.com>
2019-12-15 17:45:20 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/victormarques/cursomc/CursomcApplication.java
+++ src/main/java/com/victormarques/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.victormarques.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.victormarques.cursomc.domain.Cidade;
 import com.victormarques.cursomc.domain.Cliente;
 import com.victormarques.cursomc.domain.Endereco;
 import com.victormarques.cursomc.domain.Estado;
+import com.victormarques.cursomc.domain.Pagamento;
+import com.victormarques.cursomc.domain.PagamentoComBoleto;
+import com.victormarques.cursomc.domain.PagamentoComCartao;
+import com.victormarques.cursomc.domain.Pedido;
 import com.victormarques.cursomc.domain.Produto;
+import com.victormarques.cursomc.domain.enums.EstadoPagamento;
 import com.victormarques.cursomc.domain.enums.TipoCliente;
 import com.victormarques.cursomc.repository.CategoriaRepository;
 import com.victormarques.cursomc.repository.CidadeRepository;
 import com.victormarques.cursomc.repository.ClienteRepository;
 import com.victormarques.cursomc.repository.EnderecoRepository;
 import com.victormarques.cursomc.repository.EstadoRepository;
+import com.victormarques.cursomc.repository.PagamentoRepository;
+import com.victormarques.cursomc.repository.PedidoRepository;
 import com.victormarques.cursomc.repository.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner  {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidioRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,6 +103,21 @@ public class CursomcApplication implements CommandLineRunner  {
 		
 		clienteRepository.save(cli1);
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null,sdf.parse("30/09/2017 10:32"),cli1,e1);
+		Pedido ped2 = new Pedido(null,sdf.parse("10/10/2017 19:35"),cli1,e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO,ped1,6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagt2 = new PagamentoComBoleto(null,EstadoPagamento.PENDENTE,ped2,sdf.parse("20/10/2017 00:00"),null); 
+		ped2.setPagamento(pagt2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidioRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagt2));
 		
 	}
 
--- src/main/java/com/victormarques/cursomc/domain/Cliente.java
+++ src/main/java/com/victormarques/cursomc/domain/Cliente.java
@@ -37,7 +37,10 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -129,6 +132,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 	
