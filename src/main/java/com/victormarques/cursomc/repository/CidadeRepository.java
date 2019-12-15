57c964213494837eccb2dd7a15ffe1cfc476499a
Victor Marques <victor14souza@gmail.com>
2019-12-14 23:53:57 -0300

    Estado e Cidade

---
+++ src/main/java/com/victormarques/cursomc/repository/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.victormarques.cursomc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victormarques.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>  {
+		
+	
+}
