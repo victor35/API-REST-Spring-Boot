99bb99f6d63928814334c5ae4e3e0b1b45c644d5
Victor Marques <victor14souza@gmail.com>
2019-12-15 15:23:11 -0300

    Endpoint/clientes/{id} disponivel

--- src/main/java/com/victormarques/cursomc/domain/Cliente.java
+++ src/main/java/com/victormarques/cursomc/domain/Cliente.java
@@ -12,9 +12,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.victormarques.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -30,6 +30,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
---
+++ src/main/java/com/victormarques/cursomc/service/ClienteService.java
@@ -0,0 +1,26 @@
+package com.victormarques.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victormarques.cursomc.domain.Cliente;
+import com.victormarques.cursomc.repository.ClienteRepository;
+import com.victormarques.cursomc.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	public ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return  obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado Id: " + id + ", Tipo : "
+				+ Cliente.class.getName()));
+	}
+}
