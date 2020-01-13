03e12f3207e440e752f449394abab78ba834da7f
Victor Marques <victor14souza@gmail.com>
2020-01-12 23:45:30 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/victormarques/cursomc/config/TestConfig.java
+++ src/main/java/com/victormarques/cursomc/config/TestConfig.java
@@ -1,11 +1,18 @@
 package com.victormarques.cursomc.config;
 
+import java.io.InputStream;
 import java.text.ParseException;
+
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 
 import com.victormarques.cursomc.service.DBService;
 import com.victormarques.cursomc.service.EmailService;
@@ -27,6 +34,60 @@ public class TestConfig {
 	@Bean
 	public EmailService emailService() {
 		return new MockEmailService();
+	}
+	
+	@Bean
+	public JavaMailSender javaMailSender() {
+		return new JavaMailSender() {
+			
+			@Override
+			public void send(SimpleMailMessage... simpleMessages) throws MailException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void send(SimpleMailMessage simpleMessage) throws MailException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void send(MimeMessage... mimeMessages) throws MailException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void send(MimeMessage mimeMessage) throws MailException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public MimeMessage createMimeMessage() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
 	}
 	
 }
