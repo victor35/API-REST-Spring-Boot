a09403088ae88783f1579c13b79025ffec34ff3f
Victor Marques <victor14souza@gmail.com>
2020-01-17 00:29:27 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/victormarques/cursomc/CursomcApplication.java
+++ src/main/java/com/victormarques/cursomc/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.victormarques.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.victormarques.cursomc.service.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner  {
-
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,7 +19,7 @@ public class CursomcApplication implements CommandLineRunner  {
 
 	@Override
 	public void run(String... args) throws Exception {
-	
+		s3Service.uploadFile("/home/victor/Imagens/teste-spring.png");
 	}
 
 }
