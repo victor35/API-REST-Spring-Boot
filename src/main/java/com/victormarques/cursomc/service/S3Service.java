18deaddf3fa353c513fc5f404840ada758b619be
Victor Marques <victor14souza@gmail.com>
2020-01-21 23:35:28 -0300

    Tratando Exceções adequadamente

--- src/main/java/com/victormarques/cursomc/service/S3Service.java
+++ src/main/java/com/victormarques/cursomc/service/S3Service.java
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.victormarques.cursomc.service.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -51,7 +52,7 @@ public class S3Service {
 			return s3client.getUrl(bucketName, fileName).toURI();
 
 		} catch (URISyntaxException e) {
-			throw new RuntimeErrorException(null, "Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 
 	}
