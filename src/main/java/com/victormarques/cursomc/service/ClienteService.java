ecb1c6d11a36c926241a82bf861fa2f500929a85
Victor Marques <victor14souza@gmail.com>
2020-01-24 00:11:22 -0300

    Endpoint para buscar Cliente por email

--- src/main/java/com/victormarques/cursomc/service/ClienteService.java
+++ src/main/java/com/victormarques/cursomc/service/ClienteService.java
@@ -93,6 +93,20 @@ public class ClienteService {
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
+	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticate();
+		if(user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Cliente obj = repo.findByEmail(email);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + ClienteService.class.getName());
+		}
+		
+		return obj;
+	}
 
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
